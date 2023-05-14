cbf4fac883b06c7fecece5c07f964f827859d2a4
Travis Arndt <tmarndt1991@gmail.com>
2023-05-14 10:40:50 -0400

    Code cleanup and added Benchmark project

---
+++ DispatchR.Benchmark/Benchmarky.cs
@@ -0,0 +1,85 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Runtime.InteropServices;
+
+namespace DispatchR.Benchmark
+{
+
+    public class Benchmarky
+    {
+        private IEnumerable<Dispatchee> _dispatchees = new Dispatchee[]
+        {
+            new FakeDispatchee(token =>
+            {
+                return Task.CompletedTask;
+            }, DispatchTime.AtFrequency(TimeSpan.FromMilliseconds(1))),
+            new FakeDispatchee(token =>
+            {
+                return Task.CompletedTask;
+            }, DispatchTime.AtFrequency(TimeSpan.FromMilliseconds(2))),
+            new FakeDispatchee(token =>
+            {
+                return Task.CompletedTask;
+            }, DispatchTime.AtFrequency(TimeSpan.FromMilliseconds(3))),
+            new FakeDispatchee(token =>
+            {
+                return Task.CompletedTask;
+            }, DispatchTime.AtFrequency(TimeSpan.FromMilliseconds(4))),
+            new FakeDispatchee(token =>
+            {
+                return Task.CompletedTask;
+            }, DispatchTime.AtFrequency(TimeSpan.FromMilliseconds(5)))
+        };
+
+        private FakeDispatcher _dispatcher;
+
+        public Benchmarky()
+        {
+            _dispatcher = new FakeDispatcher(_dispatchees);
+        }
+
+        public void Run()
+        {
+            _dispatcher.Dispatch();
+        }
+
+        [Benchmark]
+        public void RunAsync()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(10);
+
+                cancellationTokenSource.Cancel();
+            });
+
+            // Act
+            _dispatcher.RunAsync(cancellationTokenSource.Token).Wait();
+        }
+    }
+
+    [DispatchOrder(1)]
+    public class FakeDispatchee : Dispatchee
+    {
+        public FakeDispatchee(Func<CancellationToken, Task> func, DispatchTime dispatchTime) : base(func, dispatchTime)
+        {
+
+        }
+    }
+
+    public class FakeDispatcher : Dispatcher
+    {
+        private readonly IEnumerable<Dispatchee> _dispatchees;
+
+        public FakeDispatcher(IEnumerable<Dispatchee> dispatchees) : base(dispatchees)
+        {
+            _dispatchees = dispatchees;
+        }
+
+        public void Dispatch()
+        {
+            Dispatch(_dispatchees.ToList());
+        }
+    }
+}
